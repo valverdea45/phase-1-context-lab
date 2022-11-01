4202178a57f03609bc39fa9f09a27002a837742a
Adrian <adrianvalverde45@gmail.com>
2022-11-01 14:32:58 -0400

    commit tree f823e096e02a9f0f8474c7e1f6e69f1018a5578c

--- index.js
+++ index.js
@@ -9,6 +9,69 @@
  for you to use if you need it!
  */
 
+
+function createEmployeeRecord(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(nestedArray){
+    const loopingThroughArrays = nestedArray.map(createEmployeeRecord)
+    return loopingThroughArrays
+}
+
+function createTimeInEvent(dateStamps){
+    
+    let [date, hour] = dateStamps.split(" ")
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date,
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamps){
+    let [date, hour] = dateStamps.split(" ")
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateOfPunch){
+    const timeInDate = this.timeInEvents.find((employeeTimeInDates => {
+        return employeeTimeInDates.date === dateOfPunch
+    }))
+    const timeOutDate = this.timeOutEvents.find((employeeTimeOutDates) => {
+        return employeeTimeOutDates.date === dateOfPunch
+    })
+    const diffrenceInHours = timeOutDate.hour - timeInDate.hour
+    const diffrenceInHourSingleDigits = diffrenceInHours/100
+    return diffrenceInHourSingleDigits
+}
+
+
+function wagesEarnedOnDate(date) {
+    const paymentOwedOnDate = hoursWorkedOnDate.call(this, date) * this.payPerHour
+    return paymentOwedOnDate
+}
+
+function findEmployeeByFirstName(arrayOfEmployee, firstNameToBeMatched){
+    const findingNames = arrayOfEmployee.find((individualEmployee) => {
+        return individualEmployee.firstName === firstNameToBeMatched
+    })
+    return findingNames
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,4 +83,16 @@ const allWagesFor = function () {
 
     return payable
 }
+
+function calculatePayroll(arrayOfEmployee){
+    
+    const grandTotalOwed = arrayOfEmployee.reduce((accum, thingBeingLooped) => {
+        return accum + allWagesFor.call(thingBeingLooped)
+    },0)
+    return grandTotalOwed
+}
+
+
+
+
 
